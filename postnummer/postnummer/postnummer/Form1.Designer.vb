b7e647e1cc0a1cd36e91ce71e328d221986864c8
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-02-28 18:56:33 +0100

    1st

---
+++ postnummer/postnummer/postnummer/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStripTextBox1 = New System.Windows.Forms.ToolStripTextBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(10, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 24)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Les fra fil"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(10, 66)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(607, 251)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(115, 6)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(168, 24)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "INSERT postnr til Databasen"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(10, 36)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(103, 24)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Clear listbox"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(115, 36)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(168, 24)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "INSERT poststed til Databasen"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(633, 357)
+        Me.TabControl1.TabIndex = 6
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Button4)
+        Me.TabPage1.Controls.Add(Me.ListBox1)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Controls.Add(Me.Button3)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(625, 331)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.ToolStrip1)
+        Me.TabPage2.Controls.Add(Me.DataGridView1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(625, 331)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Se databsen"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(619, 325)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripSeparator1, Me.ToolStripLabel1, Me.ToolStripTextBox1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(3, 3)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(619, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(81, 22)
+        Me.ToolStripButton1.Text = "Se Databasen"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(32, 22)
+        Me.ToolStripLabel1.Text = "Søk: "
+        '
+        'ToolStripTextBox1
+        '
+        Me.ToolStripTextBox1.Name = "ToolStripTextBox1"
+        Me.ToolStripTextBox1.Size = New System.Drawing.Size(100, 25)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(633, 357)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents ToolStripTextBox1 As ToolStripTextBox
+End Class
---
+++ postnummer/postnummer/postnummer/Form1.vb
@@ -0,0 +1,177 @@
+﻿
+Imports System.ComponentModel
+Imports MySql.Data.MySqlClient
+
+
+Public Class Form1
+
+    Dim command As MySqlCommand
+
+    Public tilkobling = New MySqlConnection("Server=localhost;Database=oving17vb;Uid=root;Pwd=root")
+
+
+    Private Function sporring(ByVal sql As String) As DataTable
+        Dim mydata As New DataTable
+        Try
+            tilkobling.Open()
+            Dim kommando As New MySqlCommand(sql, tilkobling)
+            Dim da As New MySqlDataAdapter
+            da.SelectCommand = kommando
+            da.Fill(mydata)
+            tilkobling.Close()
+        Catch ex As Exception
+            MessageBox.Show("Noe gikk galt. " & ex.Message)
+            tilkobling.Close()
+        End Try
+        Return mydata
+
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For Each line In System.IO.File.ReadAllLines("postnr.txt")
+            ListBox1.Items.Add(line)
+        Next
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Clear()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim filnavn = "postnr.txt"
+        Dim filnummer = FreeFile()
+        FileOpen(filnummer, filnavn, OpenMode.Input)
+
+        While Not EOF(filnummer)
+            Dim linje = LineInput(filnummer)
+
+            Dim kolonner = linje.Split(vbTab)
+
+            Dim postnr = kolonner(0)
+            Dim postnrOppd = postnr.Split(vbTab)
+            Dim poststed = postnrOppd(postnrOppd.Length - 1)
+            Dim nr = postnrOppd(0)
+            For indeks = 0 To postnrOppd.Length - 2
+                nr = nr & " " & postnrOppd(indeks)
+            Next
+            ListBox1.Items.Add(nr)
+            'ListBox1.Items.Add(poststed)
+
+
+            tilkobling.Open()
+            Dim sql As New MySqlCommand("insert into postnummer (postnummer) values (@postnummer)", tilkobling)
+            sql.Parameters.AddWithValue("@postnummer", nr)
+
+            sql.ExecuteNonQuery()
+            'Dim ansattID = sql.LastInsertedId
+            tilkobling.close()
+
+        End While
+
+
+
+        FileClose(filnummer)
+
+
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim filnavn = "postnr.txt"
+        Dim filnummer = FreeFile()
+        FileOpen(filnummer, filnavn, OpenMode.Input)
+
+        While Not EOF(filnummer)
+            Dim linje = LineInput(filnummer)
+
+            Dim kolonner = linje.Split(vbTab)
+
+            Dim postnr = kolonner(1)
+            Dim postnrOppd = postnr.Split(vbTab)
+            Dim poststed = postnrOppd(postnrOppd.Length - 1)
+            Dim nr = postnrOppd(0)
+            For indeks = 0 To postnrOppd.Length - 1
+                nr = nr & " " & postnrOppd(indeks)
+            Next
+
+            ListBox1.Items.Add(poststed)
+            tilkobling.Open()
+            Dim sql As New MySqlCommand("insert into poststed (poststed) values (@poststed)", tilkobling)
+            sql.Parameters.AddWithValue("@poststed", poststed)
+
+            sql.ExecuteNonQuery()
+
+            tilkobling.close()
+        End While
+
+
+
+        FileClose(filnummer)
+
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        Dim dbdataset As New DataTable
+        Dim bsource As New BindingSource
+        Dim SDA As New MySqlDataAdapter
+
+
+        Try
+            tilkobling.open()
+            Dim query As String
+            query = "select postnummer, poststed from postnummer, poststed where postnummer.id = poststed.id order by postnummer asc"
+            Command = New MySqlCommand(query, tilkobling)
+            SDA.SelectCommand = command
+            SDA.Fill(dbdataset)
+            bsource.DataSource = dbdataset
+            DataGridView1.DataSource = bsource
+            SDA.Update(dbdataset)
+            tilkobling.close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            tilkobling.dispose()
+
+        End Try
+    End Sub
+
+    Private Sub ToolStripTextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles ToolStripTextBox1.KeyDown
+
+        Dim sok = ToolStripTextBox1.Text
+
+        Select Case e.KeyCode
+            Case Keys.Enter
+
+                Dim dbdataset As New DataTable
+                Dim bsource As New BindingSource
+                Dim SDA As New MySqlDataAdapter
+
+
+                Try
+                    tilkobling.open()
+                    Dim query As String
+                    query = "SELECT postnummer, poststed from postnummer, poststed where postnummer.id = poststed.id and (postnummer.postnummer like '%" & sok & "%' or poststed.poststed like '%" & sok & "%');"
+                    command = New MySqlCommand(query, tilkobling)
+                    SDA.SelectCommand = command
+                    SDA.Fill(dbdataset)
+                    bsource.DataSource = dbdataset
+                    DataGridView1.DataSource = bsource
+                    SDA.Update(dbdataset)
+                    tilkobling.close()
+                Catch ex As Exception
+                    MessageBox.Show(ex.Message)
+                Finally
+                    tilkobling.dispose()
+
+                End Try
+
+
+
+        End Select
+    End Sub
+End Class
